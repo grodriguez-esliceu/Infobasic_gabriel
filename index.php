877d69d632b74cb35c79d4f139dc60620d078367
grodriguez-esliceu <grodriguezh@esliceu.net>
2026-02-05 16:08:38 +0100

    Entradas

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-este es el index
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi página web</title>
+</head>
+<body>
+<h1>Bienvenido a mi web</h1>
+<?php
+while (have_posts()) {
+    the_post();
+    ?>
+    <h2><a href="<?= the_permalink()?>"><?= the_title()?></a></h2>
+    <p><?= the_content()?></p>
+<?php
+}
+?> 
+
+</body>
+</html>
